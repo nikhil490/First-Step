02535563d52195e58658c8c8ba55b80f9e19dbe4
nikhil baby <nikhil.baby@stud.th-deg.de>
2019-10-01 10:02:06 +0200

    Implementing stochastic gradient descent using Back-propagation Algorithm.

--- src/training1.py
+++ src/training1.py
@@ -7,29 +7,87 @@ class Network:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.bias = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weight = [np.random.randn(y, x) for y, x in zip(sizes[:-1], sizes[1:])]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(x, y) for y, x in zip(sizes[:-1], sizes[1:])]
 
     def sigmoid(self, z):
         return 1 / 1+np.exp(z)
 
-    def sgd(self, training_data, eta, epoch, mini_length,test_data = None):
-        if test_data:
-            n_test = len(test_data)
+    def sgd(self,  training_data,epochs, mini_length, eta, test_data):
+        training_data = list(training_data)
         n_length = len(training_data)
-        for j in epoch:
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+        for j in range(epochs):
             random.shuffle(training_data)
             mini_batch = [training_data[k:k+mini_length] for k in range(0, n_length, mini_length)]
             for minbatch in mini_batch:
                 self.update_mini_batch(minbatch, eta)
             if test_data:
-                print("Generation {0} : {1}/{2}".format(j, self.evaluate(test_data, n_test)))
+                print("Epoch {0} : {1}/{2}".format(j, self.evaluate(test_data), n_test))
             else:
-                print("Generation {} is complete".format(j))
+                print("Epoch {} is complete".format(j))
 
-    def update_mini_batch(self,minbatch,eta):
+    def update_mini_batch(self, minbatch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in minbatch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta / len(minbatch)) * nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(minbatch)) * nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x]  # list to store all the activations, layer by layer
+        zs = []  # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+    def vectorized_result(j):
+        e = np.zeros((10, 1))
+        e[j] = 1
+        return e
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations - y)
+
+    def sigmoid_prime(self,z):
+        """Derivative of the sigmoid function."""
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a)+b)
+        return a
 
 
+#netwerk = Network([784, 30, 10])
+#training_data, validation_data, test_data = unwrap.load_wrapper()
+#netwerk.sgd(training_data, 30, 10, 3.0, test_data=test_data)
 
-
-netwerk = Network([784, 30, 10])
